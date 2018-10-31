02c8ceb1d789778de755df808adef14d01d6b133
Josh S <joshshigaki@users.noreply.github.com>
2018-10-30 18:20:49 -0700

    assignment push

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import mission_to_mars
+import pymongo
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
-    listings = mongo.db.listings.find_one()
-    return render_template("index.html", listings=listings)
+    mars = mongo.db.mars_db.find_one()
+    print(mars)
+    return render_template("index.html", mars=mars)
 
-
-@app.route("/scrape")
-def scraper():
-    listings = mongo.db.listings
-    listings_data = mission_to_mars.scrape()
-    listings.update({}, listings_data, upsert=True)
+@app.route('/scrape')
+def scrape():
+     # db.collection.remove()
+    mars = mongo.db.mars_db
+    mars_data = mission_to_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
+
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
--- mission_to_mars.py
+++ mission_to_mars.py
@@ -1,22 +1,110 @@
+
 import pandas as pd
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
+from selenium import webdriver
 
-def init_browser():
+#initiate browser
+def initialize_browser():
     executable_path = {"executable_path": "chromedriver.exe"}
-    return Browser("chrome", **executable_path, headless=False)
+    return Browser("chrome", **executable_path, headless=True)
 
+#scraping Mars News for latest headline title and body
 def scrape():
-    browser = init_browser()
-    listings = {}
-    
+    browser = initialize_browser()
+
+    mars_data = {}
+
     mars_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
     browser.visit(mars_url)
-    
+
     html = browser.html
     soup = bs(html, "html.parser")
-    
-    listings["headline"] = soup.find("a", class_="content-title").get_text()
-    listings["headline_body"] = soup.find("div", class_="article_teaser_body").get_text()
-    
-    return listings
+
+    mars_data['recent_title'] = soup.find("div",class_="content_title").text
+    mars_data['recent_body'] = soup.find("div", class_="article_teaser_body").text
+
+#scrape for JPL Mars Space Images - Featured Image
+
+    pic_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    nasa_link = 'https://www.jpl.nasa.gov'
+
+    browser.visit(pic_url)
+    xpath_mars_link = '//*[@id="full_image"]'
+    xpath_mars_pic = '//*[@id="fancybox-lock"]/div/div[1]/img'
+
+    #store path of link and then click
+    mars_results = browser.find_by_xpath(xpath_mars_link)
+    mars_link = mars_results[0]
+    mars_link.click()
+
+    #read page with BeautifulSoup to obtain link
+    #html = browser.html
+    #soup = bs(html, 'html.parser')
+
+    #retrieve partial link
+    #pic_link = soup.find("img", class_="fancybox-image")["src"]
+    pic_link = browser.find_by_xpath(xpath_mars_pic)
+
+    #create link
+    mars_pic_link = nasa_link + pic_link.text
+    mars_data["mars_pic_link"] = mars_pic_link
+
+#scrape for Mars Weather information
+
+    mars_weather_link = 'https://twitter.com/marswxreport?lang=en'
+
+    browser.visit(mars_weather_link)
+    html = browser.html
+    soup = bs(html, "html.parser")
+    mars_weather = soup.find("p", class_="TweetTextSize TweetTextSize--normal js-tweet-text tweet-text").text
+
+
+    #xpath_mars_weather = '//*[@id="stream-item-tweet-1055479145478737921"]/div[1]/div[2]/div[2]/p'
+
+
+    #weather_results = browser.find_by_xpath(xpath_mars_weather)
+    #weather_body = weather_results[0]
+    #mars_weather_body = weather_body.text
+    mars_data["mars_weather"] = mars_weather
+
+
+#use Pandas to scrape Mars facts
+
+    mars_facts_url = 'https://space-facts.com/mars/'
+    mars_facts_table = pd.read_html(mars_facts_url)
+
+    #convert table into a dataframe
+    mars_facts_df = mars_facts_table[0]
+
+    #convert dataframe into html
+    mars_facts_df.to_html('mars_facts.html')
+
+
+#scrape for titles and pictures of Mars Hemispheres
+
+    mars_high_res_link = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    base_link = 'https://astrogeology.usgs.gov/'
+    hemisphere_image_urls = []
+
+    browser.visit(mars_high_res_link)
+    soup = bs(browser.html, "html.parser")
+    hemispheres = soup.find_all("div", class_="item")
+
+    for hemisphere in hemispheres:
+        
+        title = hemisphere.find("a", class_="itemLink").find("img")["alt"]
+        title_edit = title[:-19]
+        hemisphere_image_urls.append({
+            "title": title_edit
+        })
+        
+        hemi_link = hemisphere.find("a", class_="itemLink")
+        image_link = hemi_link["href"]
+        full_link = f'{base_link}{image_link}'
+        hemisphere_image_urls.append({
+            "img_url": full_link
+        })
+    mars_data["hemisphere_image_urls"] = hemisphere_image_urls
+
+    return mars_data
